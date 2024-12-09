585c0ac55bb73338b6d5f7fc37a719f7c75f8960
Amisha Halarnkar <amishahalarnkar@gmail.com>
2024-12-09 09:57:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+    const navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
